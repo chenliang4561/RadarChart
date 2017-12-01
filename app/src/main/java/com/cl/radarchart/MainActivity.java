ca12af4facf3a2d6a6436918e86b1e067f9b2b01
chenliang4561 <chenliang4561@163.com>
2017-12-01 16:15:20 +0800

    首次提交

---
+++ app/src/main/java/com/cl/radarchart/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cl.radarchart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * @author cliang
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
